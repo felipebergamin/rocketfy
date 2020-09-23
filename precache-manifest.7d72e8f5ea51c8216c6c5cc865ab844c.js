01f835cfeb5d6afcc1a17e58b736e95e8f26802d
Felipe Bergamin <felipebergamin6@gmail.com>
2020-09-23 18:47:18 -0300

    Updates

---
+++ precache-manifest.7d72e8f5ea51c8216c6c5cc865ab844c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ffc0a145adbdee37bfe99064f7d2cc6",
+    "url": "/rocketfy/index.html"
+  },
+  {
+    "revision": "d7ae320d5a67c5f74136",
+    "url": "/rocketfy/static/js/2.8a837652.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rocketfy/static/js/2.8a837652.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4426171f8720d93f129",
+    "url": "/rocketfy/static/js/main.dce32c43.chunk.js"
+  },
+  {
+    "revision": "73752f21ba866f4ab99c",
+    "url": "/rocketfy/static/js/runtime-main.4ce62326.js"
+  }
+]);
